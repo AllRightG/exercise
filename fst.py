669103d3150105c83d9ced29550f65d05830862e
AllRightG <sge31@yandex.ru>
2023-03-09 14:11:36 +0300

    Сумма чет. чисел от 1 до 15

--- fst.py
+++ fst.py
@@ -1,3 +1,13 @@
-#Вывести квадрат числа
+'''#Вывести квадрат числа
 num = int(input('Введите число: '))
-print((lambda num: num**2)(num))
+print((lambda num: num**2)(num))'''
+
+'''#Task 1. Найти сумму всех четных чисел от 1 до 15
+num1 = 1
+num2 = 15
+result = 0
+for i in (num1, num2+1):
+	if i%2==0:
+		result +=i
+print(result)
+'''
