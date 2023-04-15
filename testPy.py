ecd908a9cc2cf5febc40762afc19e1244c06cc68
AllRightG <sge31@yandex.ru>
2023-04-15 16:20:31 +0500

    Команды модуля math

---
+++ testPy.py
@@ -0,0 +1,11 @@
+import math as m
+a = int(input('Введите первое число '))
+b = int(input('Введите второе число '))
+
+print (a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(int(a/b))
+print(a%b)
+m.sqrt()
