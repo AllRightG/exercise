b19970a5fb2b30bb812cd97e1fa5646cb3d38e41
AllRightG <sge31@yandex.ru>
2023-04-23 10:37:48 +0500

    Конец базы ооп

--- OOP.py
+++ OOP.py
@@ -1,6 +1,6 @@
 #ООП:
 
-#Класс - шаблон для начала объекта
+#Класс - шаблон для начала объекта, некий чертеж для создания объектов
 #Объект - сущность которое обладает поведением и состояние класса, реализация какого-либо класса
 #Свойства - переменные внутри класса которые его описывают
 #Функции - методы класса
@@ -28,12 +28,46 @@
 # x = fun.material
 # print(x)
 
-class tableClass:
-  __color = ''
+"""
+class Color:
+     red = 0
+     green = 0
+     blue = 0
 
-  def __init__(self, mat):
-  #self.color = color
-    self.mat = mat
+     def __init__(self, r,g,b):
+        red = r
+        green = g
+        blue = b
+
+     def toHex(self):
+          return '#%02x%02x%02x' % (red, green, blue)
+
+class ColorAlpha(Color):
+  alpha = 1
+
+  def __init__(self,r,g,b,a):
+    red = r
+    green = g
+    blue = b
+    alpha = a
+
+gray = Color(80, 80, 80) #серый цвет
+
+red = ColorAlpha(255, 0,0,.5) #Наполовину непрозрачный красный цвет
+ """
+
+""" class tableClass:
+  __color = 'Not Color'
+
+  def __init__(self, color, mat):
+    self.color = color
+    self.__mat = mat
+
+  def get_mat(self):
+    return self.__mat
+
+  def set_mat(self, mat):
+    self.__mat = mat
 
   @staticmethod
   def testStMet(color):
@@ -47,5 +81,6 @@ class tableClass:
     tmp = cls('metal')
     return tmp
 
-redT = tableClass.testClsMet()
-print(redT.mat)
+redT = tableClass('red', 'wood')
+#print(redT.__mat)
+print(retT.color) """
